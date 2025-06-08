56c1f11496add25e3941863b42b97d4d122e46d7
WidyBangkitJuniar <widibangkitjuniar@gmail.com>
2025-06-09 00:55:02 +0700

    public

--- src/Barang.java
+++ src/Barang.java
@@ -6,17 +6,22 @@ public class Barang {
     public String tanggalBerakhir;
     public int stok;
 
-    public void Barang(String kode, String nama, double harga, double hargaDiskon, String tanggalBerakhir, int stok) {
+    public Barang(String kode, String nama, double harga, double hargaDiskon, String tanggalBerakhir, int stok) {
         this.kode = kode;
         this.nama = nama;
         this.harga = harga;
-        this.hargaDiskon = harga * (1 - hargaDiskon / 100);
+        this.hargaDiskon = hargaDiskon;
         this.tanggalBerakhir = tanggalBerakhir;
         this.stok = stok;
 
-    Sandy alfaizal
-    Iqbal nurfikri
-    Widy bangkit juniar
+        public Barang(String kode, String nama, double harga, int stok) {
+            this.kode = kode;
+            this.nama = nama;
+            this.harga = harga;
+            this.hargaDiskon = 0.0;
+            this.tanggalBerakhir = "";
+            this.stok = stok;
+        }
     }
 
 }
