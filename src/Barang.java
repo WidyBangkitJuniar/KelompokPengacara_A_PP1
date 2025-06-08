e1f19b51056b36b9a0a0ac0f3376d9960edd0139
WidyBangkitJuniar <widibangkitjuniar@gmail.com>
2025-06-08 20:09:09 +0700

    Barang

---
+++ src/Barang.java
@@ -0,0 +1,21 @@
+public class Barang {
+    public String kode;
+    public String nama;
+    public double harga;
+    public double hargaDiskon;
+    public String tanggalBerakhir;
+    public int stok;
+
+    public void Barang(String kode, String nama, double harga, double hargaDiskon, String tanggalBerakhir, int stok) {
+        this.kode = kode;
+        this.nama = nama;
+        this.harga = harga;
+        this.hargaDiskon = harga * (1 - hargaDiskon / 100);
+        this.tanggalBerakhir = tanggalBerakhir;
+        this.stok = stok;
+
+        
+
+    }
+
+}
